30222e7069551355f5cf5db5aecad10f0917b128
Sahilkhatri68 <86000524+Sahilkhatri68@users.noreply.github.com>
2022-02-06 22:47:01 +0530

    initial commit's

--- OTP.js
+++ OTP.js
@@ -7,22 +7,16 @@ function Generate() {
 
   for (let i = 0; i < 4; i++) {
     OTP1 = num[Math.floor(Math.random() * 10)];
+    OTP2 = num[Math.floor(Math.random() * 10)];
+    OTP3 = num[Math.floor(Math.random() * 10)];
+    OTP4 = num[Math.floor(Math.random() * 10)];
   }
   document.getElementById("otp1").innerHTML = OTP1;
 
-  for (let i = 0; i < 4; i++) {
-    OTP2 = num[Math.floor(Math.random() * 10)];
-  }
   document.getElementById("otp2").innerHTML = OTP2;
 
-  for (let i = 0; i < 4; i++) {
-    OTP3 = num[Math.floor(Math.random() * 10)];
-  }
   document.getElementById("otp3").innerHTML = OTP3;
 
-  for (let i = 0; i < 4; i++) {
-    OTP4 = num[Math.floor(Math.random() * 10)];
-  }
   document.getElementById("otp4").innerHTML = OTP4;
 }
 Generate();
